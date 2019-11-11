76b89f8827855bc232f4927aa1db0df616d515ca
Paulo Coto <pbarca@hotmail.com>
2019-11-11 17:36:29 +0000

    cleanup

--- CalculadoraPOO/Calculadora.cs
+++ CalculadoraPOO/Calculadora.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CalculadoraPOO
+﻿namespace CalculadoraPOO
 {
     class Calculadora
     {
-    private string visor;
+        private string visor;
         public string input
         {
             set { visor = value; }
