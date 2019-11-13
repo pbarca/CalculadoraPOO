eb92c9b028e11630dc8ec35f41c01e41aad12e31
Paulo Coto <pbarca@hotmail.com>
2019-11-13 13:00:30 +0000

    1311

--- CalculadoraPOO/Calculadora.cs
+++ CalculadoraPOO/Calculadora.cs
@@ -4,8 +4,9 @@ namespace CalculadoraPOO
 {
     class Calculadora
     {
-        private string visor;
-        private double mem;
+        private string visor, operador = "";
+        private double mem, anterior;
+        private bool op;
 
         public Calculadora()
         {
@@ -19,24 +20,60 @@ namespace CalculadoraPOO
                 if (visor == "") visor = "0";
                 switch (value)
                 {
-                    case "OFF": visor = ""; break;
+                    case "OFF": visor = ""; mem = 0; break;
                     case "AC": visor = "0"; mem = 0; break;
                     case "C": visor = "0"; break;
-                    case "MC":mem = 0; break;
+                    case "MC": mem = 0; break;
                     case "MR": visor = mem.ToString(); break;
                     case "M-": mem -= ecra; break;
                     case "M+": mem += ecra; break;
-                    case "v":visor = Math.Sqrt(ecra).ToString(); break;
-                    case "%":; break;
-                    case "+/-":visor = (-ecra).ToString(); break;
+                    case "v": visor = Math.Sqrt(ecra).ToString(); break;
+                    case "%": visor = (anterior * ecra / 100).ToString(); break;
+                    case "+/-": visor = (-ecra).ToString(); break;
                     case "/":
                     case "x":
                     case "-":
                     case "+":
-                    case "=":; break;
-                    case ".": if (!visor.Contains(",")) visor += ","; break;
-                    default: visor += value; break;
+                    case "=": operar = value; break;
+                    default: adicionar = value; break;
                 }
+            }
+        }
+
+        private string operar
+        {
+            set
+            {
+                if (operador == "") anterior = ecra;
+                if (!op) switch (operador)
+                    {
+                        case "+": anterior += ecra; break;
+                        case "-": anterior -= ecra; break;
+                        case "x": anterior *= ecra; break;
+                        case "/": anterior /= ecra; break;
+                        case "=": anterior = ecra; break;
+                    }
+                visor = anterior.ToString();
+                operador = value;
+                op = true;
+            }
+        }
+        private string adicionar
+        {
+            set
+            {
+                if (op)
+                {
+                    visor = "0";
+                    op = false;
+                }
+                if (value == ".")
+                {
+                    if (!visor.Contains(",")) visor += ",";
+                    return;
+                }
+                if (visor == "0") visor = value;
+                else visor += value;
             }
         }
         private double ecra
